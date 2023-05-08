9860cae614b0f83459a6fa79cf941cdb9d90c645
fran329 <cuentafacultad21@gmail.com>
2023-05-08 19:50:56 +0200

    actualizacion 4

---
+++ script/var.js
@@ -0,0 +1,23 @@
+document.getElementById("button mx-4");
+button1.addEventListener("click, navigateToPage2");
+function navigateToPage2 (){
+  window.location.href = "variedades.html";
+}
+
+document.getElementById("button mx-4");
+button2.addEventListener("click, navitgateToPage2");
+function navigateToPage2 (){
+  window.location.href= "contacto.html";
+}
+
+document.getElementById("button mx-4");
+button3.addEventListener("click, navigateToPage2");
+function navigateToPage2 (){
+  window.location.href="sobreurrku.html";
+}
+
+document.getElementById("button mx-4");
+button4.addEventListener("click, navigateToPage2");
+function navigateToPage2 (){
+  window.location.href="inicio.html";
+}
