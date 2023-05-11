83b4a6f68c8f24c7a55a0fd88ef11a0d13aa5bdb
fran329 <cuentafacultad21@gmail.com>
2023-05-11 13:13:37 +0200

    changes 6

---
+++ script/pics.js
@@ -0,0 +1,105 @@
+const image = document.getElementById("arbol1");
+  const largeImageSrc ="https://lh3.googleusercontent.com/RBZOGxKMJJDIbzRxe_Ev8tCwSk0fLM_jIbDTRsPUOHfMTr4MBM4ikcrbTT5wJb8ciOnaov2Zrn71P9vJxHWVfgUbthUP8OpKET1sZHcQcvlGL3kBDk9RFT0tB0gBRRNrFe5X2XVDhg=w2400";
+
+  image.addEventListener("mouseover", function() {
+    image.src = largeImageSrc;
+  });
+
+  const originalImageSrc = image.src;
+
+  image.addEventListener("mouseout", function() {
+  image.src = originalImageSrc;
+  });
+
+
+
+  const secondImage = document.getElementById("myImage");
+const largeImageSrc2="https://lh3.googleusercontent.com/RBZOGxKMJJDIbzRxe_Ev8tCwSk0fLM_jIbDTRsPUOHfMTr4MBM4ikcrbTT5wJb8ciOnaov2Zrn71P9vJxHWVfgUbthUP8OpKET1sZHcQcvlGL3kBDk9RFT0tB0gBRRNrFe5X2XVDhg=w2400";
+secondImage.addEventListener("mouseover", function (){
+secondImage.src = largeImageSrc2;  
+});
+
+const originalImageSrc2 = secondImage.src;
+
+secondImage.addEventListener("mouseout", function() {
+secondImage.src = originalImageSrc2;
+});
+
+
+const thirdImage = document.getElementById ("arbol10");
+const largeImageSrc3 ="https://lh3.googleusercontent.com/AV19XabtubEoKsfFE9zJA9RjavIu_oB9Ku54U2JQTjwuqSVOYTCClQFZ_0vkoicFcVYfhQVZAcuzLdO3X6MObYkg1GchnLhyPxX1SA6BhDwBpcGfC7T3BYhCsu1_pA4_Hso8yTwc-w=w2400";
+
+thirdImage.addEventListener("mouseover", function(){
+  thirdImage.src = largeImageSrc3;
+});
+
+const originalImageSrc3 = thirdImage.src;
+
+thirdImage.addEventListener("mouseout", function(){
+  thirdImage.src = originalImageSrc3;
+});
+
+
+const fourthImage = document.getElementById ("arbol11");
+const largeImageSrc4 = "https://lh3.googleusercontent.com/iv18DadzuA1zJtUOsKliZwplUyrpUuR525apsCXG2L-fB7pd640h_VGp0vbuLZk8KwmiFS2jI1iOIYEIQAd1Qm-IhzOOcr6FbWHn8BpNuMFFb8fSC_ZOxqMetqz5J_S8G_NA6bWdrA=w2400";
+
+fourthImage.addEventListener("mouseover", function (){
+  fourthImage.src = largeImageSrc4;
+});
+
+const OriginalImageSrc4 = fourthImage.src
+fourthImage.addEventListener ("mouseout", function () {
+  fourthImage.src = OriginalImageSrc4;
+});
+
+const fifthImage = document.getElementById("arbol5");
+const largeImageSrc5 ="https://lh3.googleusercontent.com/LCTz__XXcBe7QXd1ce5LBm_99os7akmV7PLVI6sw5a8e-32Xw822WDVK3N0PqiFRToKPWhAqZ7e3twbRQDl2jopdGtMTONH9B6lZi7G3KsViZXWxeqt-MLcY0Amqo_XJL4zJzIZhcQ=w2400";
+
+fifthImage.addEventListener("mouseover", function() {
+  fifthImage.src = largeImageSrc5;
+});
+
+const originalImageSrc5 = fifthImage.src;
+fifthImage.addEventListener("mouseout", function() {
+  fifthImage.src = originalImageSrc5;
+});
+
+const sextImage = document.getElementById("arbol12");
+const largeImageSrc6 = "https://lh3.googleusercontent.com/zv57lPpyfov91o008TH8TvqbD52o2zzQ2Z35qomkciXwJPPUrwhWcIUq0_d9uPnS9LbuZQKm-ih_nXcpuCE3qbjCdaFax18d0JUM50noxK3QOszplTDZk7G1Pur9_n4yVOuqdO01NQ=w2400";
+
+sextImage.addEventListener("mouseover", function () {
+  sextImage.src = largeImageSrc6;
+});
+
+const originalImageSrc6 = sextImage.src;
+sextImage.addEventListener("mouseout", function () {
+sextImage.src = originalImageSrc6;
+});
+
+
+const firstImage = document.getElementById("arbol1");
+const largeImageSrc1 ="https://lh3.googleusercontent.com/NTOOmJp01knGk-rM-jqU5_lfj51jPes7sCBmwZ5KN1pm_Fna3E6RuhtoKH8vzoMe04GrK8z-Gl6jp5Xsvr0wk2Df4LSElCUcbNKTHGmWahKqcghzL2C9fvdf_j46ZtSkej7J-Nnv0g=w2400";
+
+firstImage.addEventListener ("mouseover", function(){
+  firstImage.src = largeImageSrc1
+});
+
+const originalImageSrc1= firstImage.src
+firstImage.addEventListener("mouseout", function(){
+  firstImage.src = originalImageSrc1
+});
+ 
+
+const lastImage = document.getElementById ("arbol20");
+const largeImageSrc20 ="https://lh3.googleusercontent.com/8ZCQU0jWbRd-0Yxrm_4Q201jT3_7SspzRLOmdfzpUof2KsDFE72Lk5F63rHIw0Ryd_EntXlkpbtMYhHPlOiz9iX5cc8FtL6xXhQaUkc-llv2gU-YBrVAxGKjUpe58nDTys7Me43VUA=w2400";
+
+lastImage.addEventListener ("mouseover", function(){
+lastImage.src= largeImageSrc20
+});
+
+const originalImageSrc20 = lastImage.src
+lastImage.addEventListener ("mouseout" ,function() {
+lastImage.src= originalImageSrc20
+});
+
+
--- sript/var.js
+++ sript/var.js
@@ -1,23 +1,28 @@
-document.getElementById("button mx-4");
+
+/*buttons*/
+
+document.getElementById("btn mx-4");
 button1.addEventListener("click, navigateToPage2");
 function navigateToPage2 (){
   window.location.href = "variedades.html";
 }
 
-document.getElementById("button mx-4");
+document.getElementById("btn mx-4");
 button2.addEventListener("click, navitgateToPage2");
 function navigateToPage2 (){
   window.location.href= "contacto.html";
 }
 
-document.getElementById("button mx-4");
+document.getElementById("btn mx-4");
 button3.addEventListener("click, navigateToPage2");
 function navigateToPage2 (){
   window.location.href="sobreurrku.html";
 }
 
-document.getElementById("button mx-4");
+document.getElementById("btn mx-4");
 button4.addEventListener("click, navigateToPage2");
 function navigateToPage2 (){
   window.location.href="inicio.html";
 }
+
+
